d2e5df542be2589019b9ee5b578e25a4caa6157e
snipergn <andrei.banita@gmail.com>
2023-05-17 14:45:13 +0300

    new commit

--- server.js
+++ server.js
@@ -9,14 +9,12 @@ const database = {
       id: "123",
       name: "John",
       email: "john@gmail.com",
-      password: "cookies",
       joined: new Date(),
     },
     {
       id: "124",
       name: "Sally",
       email: "sally@gmail.com",
-      password: "bananas",
       joined: new Date(),
     },
   ],
@@ -37,7 +35,10 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    const { email, name, password} = req.body
+    const { email, name, password } = req.body
+    bcrypt.hash(password, null, null, function(err, hash) {
+      // Store hash in your password DB.
+  });
     database.users.push({
         id: "125",
         name: name,
@@ -47,6 +48,32 @@ app.post("/register", (req, res) => {
     })
     res.json(database.users[database.users.length-1])
 });
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params
+  let found = false;
+  database.users.forEach(users => {
+    if (users.id === id) {
+      found = true
+      return res.json(users)
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found')
+  }
+})
+bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+  // Store hash in your password DB.
+});
+// Load hash from your password DB.
+bcrypt.compare(myPlaintextPassword, hash).then(function(result) {
+  // result == true
+});
+bcrypt.compare(someOtherPlaintextPassword, hash).then(function(result) {
+  // result == false
+});
+
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
@@ -57,6 +84,7 @@ app.listen(3000, () => {
 /register --> POST = user
 /profile --> :userId --> GET = user
 /favorites --> POST = user
+/favorites --> PUT = update user
 /favorites --> GET = user
 /favorites --> DELETE = user
 
